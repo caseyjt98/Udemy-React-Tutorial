156058a9fc109b5aec6476f4a94d6c090da7f8cc
Casey Taylor <caseyjt98@gmail.com>
2019-04-20 19:33:09 -0700

    introduce state property

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Casey', age: 20 },
+      { name: 'Claire', age: 21 },
+      { name: 'Elana', age: 21 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi, I'm a react app </h1>
         <p> React is cool </p>
-        <Person name="Casey" age="20" > My Hobbies: Running </Person>
-        <Person name="Claire" age="21" />
+        <button> Switch Name </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} > My Hobbies: Running </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
