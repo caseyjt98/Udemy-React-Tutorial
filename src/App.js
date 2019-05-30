0c721429ccd5951750b6fcbc9c7eca57c242b681
Casey Taylor <caseyjt98@gmail.com>
2019-05-30 11:45:12 -0700

    two way binding, state management, styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// class component 
+// stateful component 
 class App extends Component {
 
   state = {
@@ -9,7 +11,33 @@ class App extends Component {
       { name: 'Casey', age: 20 },
       { name: 'Claire', age: 21 },
       { name: 'Elana', age: 21 }
-    ]
+    ],
+
+    otherState: 'some other value'
+  }
+
+  // event handler function for button click, pass as a reference to onClick
+  switchNameHandler = (newName) => {
+    // DON'T DO THIS: this.state.persons[0] = 'Quesarito';
+    // setState will merge the new state with the old state
+    this.setState({
+      persons: [
+        { name: newName, age: 20 },
+        { name: 'Claire', age: 21 },
+        { name: 'Elana', age: 21 }
+      ]
+    })
+  }
+
+  // event handler for input onChange 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Casey', age: 20 },
+        { name: event.target.value, age: 21 },
+        { name: 'Elana', age: 21 }
+      ]
+    })
   }
 
   render() {
@@ -17,10 +45,18 @@ class App extends Component {
       <div className="App">
         <h1> Hi, I'm a react app </h1>
         <p> React is cool </p>
-        <button> Switch Name </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} > My Hobbies: Running </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={this.switchNameHandler.bind(this, 'Quesarito')}> Switch Name </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Quesarito!!')} > My Hobbies: Running </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
